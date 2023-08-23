4eeb7eeb9ebecbd1f49bd317249ec06231f5ffaf
Mackley <mackleymagalhaess@hotmail.com>
2023-08-22 23:55:09 -0300

    Create a Widget

---
+++ DesignCodeWidget/DesignCodeWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeWidgetBundle.swift
+//  DesignCodeWidget
+//
+//  Created by Mack Pessoal on 22/08/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DesignCodeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DesignCodeWidget()
+        DesignCodeWidgetLiveActivity()
+    }
+}
