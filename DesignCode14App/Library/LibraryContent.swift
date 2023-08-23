b164498e8ae8a0ea7e945c77ac1573e93d1cbade
Mackley <mackleymagalhaess@hotmail.com>
2023-08-22 23:34:23 -0300

    Library Content and Modifiers

---
+++ DesignCode14App/Library/LibraryContent.swift
@@ -0,0 +1,34 @@
+//
+//  LibraryContent.swift
+//  DesignCode14App
+//
+//  Created by Mack Pessoal on 22/08/23.
+//
+
+import SwiftUI
+
+struct LibraryContent: LibraryContentProvider {
+    @LibraryContentBuilder
+    var views: [LibraryItem] {
+        LibraryItem(ClouseButton(), title: "Close Button View", category: .control)
+    }
+    
+    @LibraryContentBuilder
+    func modifiers(base: Image) -> [LibraryItem] {
+        LibraryItem(
+            base.cardStyle(color: Color.blue, cornerRadius: 22),
+            title: "Card Style",
+            category: .effect
+        )
+    }
+    
+}
+
+extension View {
+    func cardStyle(color: Color, cornerRadius: CGFloat) -> some View {
+        return self
+            .background(color)
+            .clipShape(RoundedRectangle(cornerRadius: cornerRadius, style: .continuous))
+            .shadow(color: color.opacity(0.3),radius: 20, x: 0, y: 10)
+    }
+}
